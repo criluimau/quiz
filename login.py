0f2e3bb0eb5d18206ec8bfc1bd7100bb8837c8b2
criluimau <maurizio64.zamp@google.com>
2012-01-07 18:11:56 +0100

    Interfaccia Grafica

--- login.py
+++ login.py
@@ -4,7 +4,9 @@
 import sys, utente
 
 try:  
-	import gtk, pygtk
+	import pygtk
+	pygtk.require("2.0")
+	import gtk
 except:  
 	print("gtk o pygtk Not Availible")
 	sys.exit(1)
@@ -13,6 +15,9 @@ class login:
 	wTree 		= None
 	FinePgm 	= False
 	UserName 	= None
+	previoussWindow = ""
+	Tree        = ""
+	TreeBox     = ""
 
 	def __init__( self, db):
 		self.db	= db
@@ -36,26 +41,38 @@ class login:
 		self.wTree.connect_signals(dic)
 		
 	def CambioWindow(self, Tree, TreeBox ):
-		if Tree == "WindowLogin":
+		self.Tree 	 = Tree
+		self.TreeBox = TreeBox
+		if self.Tree == "WindowLogin":
 			self.wTree.get_object("WindowLogin").set_sensitive(1)
 			self.wTree.get_object("WindowLogin").set_visible(1)
 			self.wTree.get_object("hboxWarning").set_visible(0)
-			if TreeBox == "Login":
+			if self.TreeBox == "Login":
 				self.wTree.get_object("hbox3").set_visible(0)
 				self.wTree.get_object("hbox5").set_visible(1)
 			else:
 				self.wTree.get_object("hbox3").set_visible(1)
 				self.wTree.get_object("hbox5").set_visible(0)
 
-		if self.previousWindow != Tree:
-			self.previousWindow= Tree	
-			self.currentWindow = self.wTree.get_object(Tree)
+
+		if self.previousWindow != self.Tree:
+			self.previousWindow= self.Tree	
+			self.currentWindow = self.wTree.get_object(self.Tree)
 
 		self.currentWindow.set_position(gtk.WIN_POS_CENTER_ALWAYS)
 		self.currentWindow.set_default_size(210, 60)
+		# mostra finestra caricata
 		self.currentWindow.show()
-			
+
 	def accedi(self, widget):
+		if self.TreeBox == "Login":
+			self.accedi1(widget)
+		elif self.TreeBox == "NewUser":
+			self.accedi2(widget)
+		else:
+			self.accedi1(widget)
+			
+	def accedi1(self, widget):
 		self.CambioWindow("WindowLogin", "Login")
 		self.UserName   = self.wTree.get_object("UserName").get_text()
 		self.Password   = self.wTree.get_object("Password").get_text()
@@ -67,21 +84,49 @@ class login:
 	
 	def newuser(self, widget):
 		self.CambioWindow("WindowLogin", "NewUser")
+		self.UserName = ""
+		self.Password = ""
+		
+	def accedi2(self, widget):
+		self.CambioWindow("WindowLogin", "NewUser")
 		self.UserName    = self.wTree.get_object("UserName").get_text()
 		self.Password    = self.wTree.get_object("Password").get_text()
 		self.PasswordNew = self.wTree.get_object("PasswordNew").get_text()
-		while len(self.UserName == 0):
+		err = False
+		if len(self.UserName) == 0:
+			err = True
 			self.wTree.get_object("hboxWarning").show()
 			self.wTree.get_object("MsgWarning").set_text("Inserisci un nome utente valido")
-		while len(self.Password) == 0:
+		elif len(self.Password) == 0:
+			err = True
 			self.wTree.get_object("hboxWarning").show()
 			self.wTree.get_object("MsgWarning").set_text("Inserisci una password valida")
-		while self.Password != self.PasswordNew or len(self.Password) == 0:
+		elif self.Password != self.PasswordNew:
+			err = True
 			self.wTree.get_object("hboxWarning").show()
 			self.wTree.get_object("MsgWarning").set_text("le due password sono diverse")
 
-		autenticato      = self.autenticati()
-		print autenticato, self.FinePgm
+		# controllo esistenza utente:
+		if not err:
+			user	 = utente.utente(self.db, self.UserName)
+			RispFun	 = user.nuovo_utente(self.UserName)
+			risp	 = RispFun[0]
+			msg      = RispFun[1]
+			if risp == False:
+				err = True
+				self.wTree.get_object("hboxWarning").show()
+				self.wTree.get_object("MsgWarning").set_text(msg)
+
+		# rieseguo il controllo di autenticazione
+		autenticato = False
+		# registrazione utente
+		if not err:
+			RispFun     = user.inserisci_utente(self.Password)
+			err         = RispFun[0]
+			msg         = RispFun[1]
+			autenticato = not err
+			if autenticato:
+				autenticato   = self.autenticati()
 		if self.FinePgm:
 			self.esci(widget)
 		elif autenticato:
